8ef2ca8bd5001c1ab5ec52adc49518887f21a271
doozles411 <cindy.phelps01@gmail.com>
2023-12-18 17:34:09 -0800

    Add HTML Webpack plugin configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
 }
