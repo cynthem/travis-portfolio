8ba52044c652c037bd34e1dad0c18301b1071347
doozles411 <cindy.phelps01@gmail.com>
2024-01-19 17:37:35 -0800

    Install and configure CSS loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -8,7 +9,8 @@ module.exports = {
         path: path.resolve(__dirname, "dist")
     },
     plugins: [
-        new HtmlWebpackPlugin({ template: path.join(__dirname, "public", "index.html")})
+        new HtmlWebpackPlugin({ template: path.join(__dirname, "public", "index.html")}),
+        new MiniCssExtractPlugin()
     ],
     devServer: {
         static: { directory: path.join(__dirname, "dist")},
@@ -20,6 +22,10 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
             }
         ]
     },
