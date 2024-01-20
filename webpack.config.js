3b9f0083c531238f0b18bfab2060e2184063158f
doozles411 <cindy.phelps01@gmail.com>
2024-01-19 17:24:08 -0800

    Install Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.join(__dirname, "public", "index.html")})
+    ],
+    devServer: {
+        static: { directory: path.join(__dirname, "dist")},
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ["*", ".js"]
     }
 }
