5b9df504367d4ff7c9fac0d036902c805b524083
doozles411 <cindy.phelps01@gmail.com>
2024-01-25 14:42:15 -0800

    Create file

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import Temp from './Tempeh';
 import NoPara from './NoPara';
+import Temp2 from './TempehTwo';
+import Temp3 from './TempehThree';
 
 function App() {
   return (
     <div className="App">
       <Temp />
       <NoPara />
+      <Temp2 />
+      <NoPara />
+      <Temp3 />
     </div>
   );
 }
--- src/components/Tempeh.js
+++ src/components/Tempeh.js
@@ -2,10 +2,8 @@ import { Parallax } from 'react-parallax';
 import arts from '../assets/images/art/2012_Anim_Summer_1280.png';
 
 const Temp = () => (
-    <Parallax className="image" bgImage={arts} strength={800}>
-        <div className='content'>
-            <span className='img-txt'>a trip to cutie town</span>
-        </div>
+    <Parallax className="image" bgImage={arts} strength={400}>
+        <div className='content'></div>
     </Parallax>
 );
 
