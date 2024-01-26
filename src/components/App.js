212fbdc45948f72b4cc5c3c0af15e1316a615251
doozles411 <cindy.phelps01@gmail.com>
2024-01-26 15:30:00 -0800

    Implement main route

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
-import Temp from './Tempeh';
-import NoPara from './NoPara';
-import Temp2 from './TempehTwo';
-import Temp3 from './TempehThree';
+import React from "react";
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <Temp />
-      <NoPara />
-      <Temp2 />
-      <NoPara />
-      <Temp3 />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+   return (
+       <div className="text-box">
+           <p>cats cats cats cats cats cats cats cats cats cats cats cats
+           </p>
+       </div>
+   )
+}
+
+export default Header;
