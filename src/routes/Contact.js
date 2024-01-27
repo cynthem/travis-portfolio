631a19e6d2247fcb32d46b0226659c1254cc2cb8
doozles411 <cindy.phelps01@gmail.com>
2024-01-26 16:54:11 -0800

    Create and export component

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Contact() {
     return (
         <div className="contact">
-            <p>contact</p>
+            contact
         </div>
-    )
+    );
 }
