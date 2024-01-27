1b95727f50d1bbb5023da30cf9c367fdb8f099e4
doozles411 <cindy.phelps01@gmail.com>
2024-01-26 18:51:42 -0800

    Add initial project details

---
+++ src/util/projectList.js
@@ -0,0 +1,40 @@
+const projectList = [
+    {
+        name: "Current", 
+        link: "placeholder"
+    },
+    {
+        name: "Spring 2018", 
+        link: "placeholder"
+    },
+    {
+        name: "Spring 2018 (Extended Look)", 
+        link: "placeholder"
+    },
+    {
+        name: "Summer 2017", 
+        link: "placeholder"
+    },
+    {
+        name: "Summer 2017 (Extended Look)", 
+        link: "placeholder"
+    },
+    {
+        name: "Winter 2015", 
+        link: "placeholder"
+    },
+    {
+        name: "Winter 2015 (Extended Look)", 
+        link: "placeholder"
+    },
+    {
+        name: "Summer 2012", 
+        link: "placeholder"
+    },
+    {
+        name: "Summer 2012 (Animation)", 
+        link: "placeholder"
+    }
+];
+
+export default projectList;
