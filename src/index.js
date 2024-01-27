04b8ce7f5ca95af2979459f03771bbb3cc1cecb8
doozles411 <cindy.phelps01@gmail.com>
2024-01-26 17:06:23 -0800

    Add Home as index

--- src/index.js
+++ src/index.js
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
             {
+                index: true,
+                element: <Home />
+            },
+            {
                 path: "home",
                 element: <Home />
             },
