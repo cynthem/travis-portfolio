5f9823a9187ab303ce5f35f5c07faf594317af19
doozles411 <cindy.phelps01@gmail.com>
2024-01-26 18:56:45 -0800

    Render home page image

--- src/components/ParallaxFull.js
+++ src/components/ParallaxFull.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Parallax } from "react-parallax";
+import imageList from "../util/imageList";
 
 export default function ParallaxFull() {
     return (
-        <Parallax className="image-full" bgImage="" strength={500}>
+        <Parallax className="image-full" bgImage={imageList[0]} strength={500}>
             <div className="parallax-full"></div>
         </Parallax>
     );
