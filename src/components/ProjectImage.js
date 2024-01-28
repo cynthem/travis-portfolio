efc0829de5e8da9afa7f5666cec4919da835c9cf
doozles411 <cindy.phelps01@gmail.com>
2024-01-28 14:11:32 -0800

    Pass image prop

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Parallax } from "react-parallax";
 import imageList from "../util/imageList";
 
-export default function ProjectImage() {
+export default function ProjectImage({ image }) {
     return (
         <Parallax className="project-image" bgImage={imageList[0]} strength={500}></Parallax>
     );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
+import uniqid from "uniqid";
 import ProjectImage from "../components/ProjectImage";
 import ProjectContainer from "../components/ProjectContainer";
+import imageList from "../util/imageList";
 
 export default function Home() {
     return (
         <div className="home">
-            <ProjectImage />
-            <ProjectContainer />
+            {imageList.map((image) => {
+                return (
+                    <ProjectImage 
+                        key={uniqid()} 
+                        image={image}
+                    />
+                );
+            })}
         </div>
     );
 }
