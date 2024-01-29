c77a567e4a46987c41f8722da85e96720805d72a
doozles411 <cindy.phelps01@gmail.com>
2024-01-29 15:24:08 -0800

    Add images to list

--- src/util/projectList.js
+++ src/util/projectList.js
@@ -1,51 +1,51 @@
 import homeMain from "../assets/images/art/Home_Background.png";
-import contactMain from "../assets/images/art/Contact_Background.png";
+import Spring2018 from "../assets/images/art/2018_Spring_1280.png";
+import Spring2018Ext from "../assets/images/art/2018_Ext_Spring_1280.png";
+import Summer2017 from "../assets/images/art/2017_Summer_1280.png";
+import Summer2017Ext from "../assets/images/art/2017_Ext_Summer_1280.png";
+import Winter2015 from "../assets/images/art/2015_Winter_1280.png";
+import Winter2015Ext from "../assets/images/art/2015_Ext_Winter_1280.png";
+import Summer2012 from "../assets/images/art/2012_Summer_1280.png";
+import Summer2012Anim from "../assets/images/art/2012_Anim_Summer_1280.png";
+
 
 const projectList = [
     {
-        name: "Current", 
-        image: homeMain,
-        link: "placeholder"
+        name1: "Current", 
+        image1: homeMain,
+        link1: "placeholder"
     },
     {
-        name: "Spring 2018", 
-        image: contactMain,
-        link: "placeholder"
+        name1: "Spring 2018", 
+        image1: Spring2018,
+        link1: "placeholder",
+        name2: "Spring 2018 (Extended Look)", 
+        image2: Spring2018Ext,
+        link2: "placeholder"
     },
     {
-        name: "Spring 2018 (Extended Look)", 
-        image: homeMain,
-        link: "placeholder"
+        name1: "Summer 2017", 
+        image1: Summer2017,
+        link1: "placeholder",
+        name2: "Summer 2017 (Extended Look)", 
+        image2: Summer2017Ext,
+        link2: "placeholder"
     },
     {
-        name: "Summer 2017", 
-        image: homeMain,
-        link: "placeholder"
+        name1: "Winter 2015", 
+        image1: Winter2015,
+        link1: "placeholder",
+        name2: "Winter 2015 (Extended Look)", 
+        image2: Winter2015Ext,
+        link2: "placeholder"
     },
     {
-        name: "Summer 2017 (Extended Look)", 
-        image: homeMain,
-        link: "placeholder"
-    },
-    {
-        name: "Winter 2015", 
-        image: homeMain,
-        link: "placeholder"
-    },
-    {
-        name: "Winter 2015 (Extended Look)", 
-        image: homeMain,
-        link: "placeholder"
-    },
-    {
-        name: "Summer 2012", 
-        image: homeMain,
-        link: "placeholder"
-    },
-    {
-        name: "Summer 2012 (Animation)", 
-        image: homeMain,
-        link: "placeholder"
+        name1: "Summer 2012", 
+        image1: Summer2012,
+        link1: "placeholder",
+        name2: "Summer 2012 (Animation)", 
+        image2: Summer2012Anim,
+        link2: "placeholder"
     }
 ];
 
