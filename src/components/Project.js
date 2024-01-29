2d18b6ada106a77b62685039d1e4b4f7ee086b56
doozles411 <cindy.phelps01@gmail.com>
2024-01-29 14:46:31 -0800

    Pass image prop

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,12 @@
 import React from "react";
+import ProjectImage from "./ProjectImage";
+import ProjectDescription from "./ProjectDescription";
 
 export default function Project({ image }) {
     return (
         <div className="project">
-        
+            <ProjectImage image={image} />
+            <ProjectDescription />
         </div>
     );
 }
