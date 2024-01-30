ff8b24430c6a879b932829b820915f840b2ef3e8
doozles411 <cindy.phelps01@gmail.com>
2024-01-30 15:54:36 -0800

    Add rest of content

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,16 +4,34 @@ import { Link, Outlet } from "react-router-dom";
 export default function App() {
     return (
         <div className="App">
+
             <div className="header">
-                this is the header
-                <Link to={`home`}>Home</Link>
-                <Link to={`resume`}>Resume</Link>
-                <Link to={`contact`}>Contact</Link>
+                <div className="header-title">
+                    <h1>Travis Phelps</h1>
+                    <h2>Cinematic Artist</h2>
+                </div>
+                <div className="header-navigation">
+                    <Link to={`home`} className="nav-link">Home</Link>
+                    <Link to={`resume`} className="nav-link">Resume</Link>
+                    <Link to={`contact`} className="nav-link">Contact</Link>
+                </div>
             </div>
-            <div className="main-content">
-                <Outlet />
+
+            <div className="main-content"><Outlet /></div>
+
+            <div className="footer">
+                <p>© 2024 Travis Phelps</p>
+                <p>Made by 
+                    <a 
+                        className="footer-link"
+                        href='https://github.com/cynthem' 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        CMP
+                    </a>
+                </p>
             </div>
-            <div className="footer">this is the footer</div>
         </div>
     );
 }
