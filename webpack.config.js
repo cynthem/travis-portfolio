28d22b6bd2423257295610ac71b01564ee94a41b
doozles411 <cindy.phelps01@gmail.com>
2024-01-29 18:00:12 -0800

    Add file configurations

--- webpack.config.js
+++ webpack.config.js
@@ -27,16 +27,12 @@ module.exports = {
                 use: ["babel-loader"]
             },
             {
-                test: /\.s[ac]ss$/i,
+                test: /.s?css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: { loader: "file-loader" }
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
