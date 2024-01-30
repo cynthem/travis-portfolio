aa3b71401896defc4a8a0cebe5e8733894d1a618
doozles411 <cindy.phelps01@gmail.com>
2024-01-29 16:56:42 -0800

    Render project details

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,9 +4,16 @@ import { Parallax } from "react-parallax";
 export default function Project({ project }) {
     return (
         <div className="project">
-            <Parallax className="project-image" bgImage={project.image} strength={500}></Parallax>
+            <Parallax className="project-image" bgImage={project.bkgrd} strength={500}></Parallax>
             <div className="project-description">
-                This is the project: {project.name}
+                <div className="description-left">
+                    <img src={project.image1} alt="Project image" />
+                    <p>Project name: {project.name1}</p>
+                </div>
+                <div className="description-right">
+                    <img src={project.image2} alt="Project image" />
+                    <p>Project name: {project.name2}</p>
+                </div>
             </div>
         </div>
     );
