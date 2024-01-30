fe2709c4ffbdc68f1d02e98752cbc74a88756535
doozles411 <cindy.phelps01@gmail.com>
2024-01-29 17:30:14 -0800

    Create font and color variables

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,14 +4,22 @@ import { Parallax } from "react-parallax";
 export default function Project({ project }) {
     return (
         <div className="project">
-            <Parallax className="project-image" bgImage={project.bkgrd} strength={500}></Parallax>
+            <Parallax className="project-image" bgImage={project.bkgrd} strength={400}></Parallax>
             <div className="project-description">
                 <div className="description-details">
-                    <img className="description-image" src={project.image1} alt="Project image" />
+                    <img 
+                        className="description-image" 
+                        src={project.image1} 
+                        alt="Project image" 
+                    />
                     <p className="description-text">Project name: {project.name1}</p>
                 </div>
                 <div className="description-details">
-                    <img className="description-image" src={project.image2} alt="Project image" />
+                    <img 
+                        className="description-image" 
+                        src={project.image2} 
+                        alt="Project image" 
+                    />
                     <p className="description-text">Project name: {project.name2}</p>
                 </div>
             </div>
