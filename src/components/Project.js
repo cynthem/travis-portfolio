30e7390e80256e06c894e191452b904f2ef719ba
doozles411 <cindy.phelps01@gmail.com>
2024-01-31 14:01:08 -0800

    Add overlay opacity styles

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Parallax } from "react-parallax";
 
 export default function Project({ project }) {
+    const overlayOpacity1 = {
+        backgroundColor: project.opacity1 || 'rgba(0, 0, 0, 0.3)'
+    }
+
+    const overlayOpacity2 = {
+        backgroundColor: project.opacity2 || 'rgba(0, 0, 0, 0.3)'
+    }
+
     return (
         <div className="project">
             <Parallax className="project-image" bgImage={project.bkgrd} strength={400}></Parallax>
@@ -19,7 +27,7 @@ export default function Project({ project }) {
                             alt="Project image" 
                         />
                     </a>
-                    <div className="image-overlay"></div>
+                    <div className="image-overlay" style={overlayOpacity1}></div>
                     <p className="description-text">{project.name1}</p>
                 </div>
                 <div className="description-details">
