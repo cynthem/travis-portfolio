8f7f8b937ea35dccfbfe33919d476c9abcc33637
doozles411 <cindy.phelps01@gmail.com>
2024-01-31 17:12:36 -0800

    Add contact grid container

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -8,36 +8,39 @@ export default function Contact() {
             <Parallax className="contact-image" bgImage={contactBackground} strength={400}>
                 <div className="transition-overlay"></div>
             </Parallax>
+
             <div className="contact-content">
-                <div className="contact-row">
-                    <p className="contact-type">EMAIL</p>
-                    <a href="mailto:travis@travisphelps.com">travis@travisphelps.com</a>
-                </div>
-                <div className="contact-row">
-                    <p className="contact-type">PHONE</p>
-                    <p className="phone">(253) 693-8362</p>
-                </div>
-                <div className="contact-row">
-                    <p className="contact-type">LINKEDIN</p>
-                    <a 
-                        className="linkedin"
-                        href="https://www.linkedin.com/in/travisphelps"
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        LINKEDIN.COM/IN/TRAVISPHELPS
-                    </a>
-                </div>
-                <div className="contact-row">
-                    <p className="contact-type">INSTAGRAM</p>
-                    <a 
-                        className="instagram"
-                        href="https://www.instagram.com/tjpdigital"
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        TJPDIGITAL
-                    </a>
+                <div className="contact-grid">
+                    <div className="contact-row">
+                        <p className="contact-type">EMAIL</p>
+                        <a href="mailto:travis@travisphelps.com">travis@travisphelps.com</a>
+                    </div>
+                    <div className="contact-row">
+                        <p className="contact-type">PHONE</p>
+                        <p className="phone">(253) 693-8362</p>
+                    </div>
+                    <div className="contact-row">
+                        <p className="contact-type">LINKEDIN</p>
+                        <a 
+                            className="linkedin"
+                            href="https://www.linkedin.com/in/travisphelps"
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >
+                            LINKEDIN.COM/IN/TRAVISPHELPS
+                        </a>
+                    </div>
+                    <div className="contact-row">
+                        <p className="contact-type">INSTAGRAM</p>
+                        <a 
+                            className="instagram"
+                            href="https://www.instagram.com/tjpdigital"
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >
+                            TJPDIGITAL
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
