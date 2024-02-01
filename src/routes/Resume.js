c7aae6ef4be2f978b47ed28651e7d44a940b582e
doozles411 <cindy.phelps01@gmail.com>
2024-01-31 18:42:55 -0800

    Add resume content

--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -8,14 +8,74 @@ export default function Resume() {
             <Parallax className="resume-image" bgImage={resumeBackground} strength={400}>
                 <div className="transition-overlay"></div>
             </Parallax>
+
             <div className="resume-content">
                 <div className="position-section">
                     <h3>Warner Bros / Avalanche Software</h3>
-                    <div className="position-details"></div>
+                    <div className="position-details">
+                        <div className="title-date">
+                            <h4 className="job-title">Master Cinematic Artist</h4>
+                            <p className="job-date">October 2019 - Current</p>
+                        </div>
+                        <div className="game-details">
+                            <h4 className="game-name">"Hogwarts Legacy"</h4>
+                        </div>
+                    </div>
                 </div>
                 <div className="position-section">
                     <h3>Warner Bros / Monolith Productions</h3>
-                    <div className="position-details"></div>
+                    <div className="position-details">
+                        <div className="title-date">
+                            <h4 className="job-title">Master Cinematic Artist</h4>
+                            <p className="job-date">February 2015 - October 2019</p>
+                        </div>
+                        <div className="game-details">
+                            <h4 className="game-name">"Middle Earth: Shadow of War"</h4>
+                            <div className="job-duties">
+                                <h5>Content</h5>
+                                <ul className="duties-list">
+                                    <li>Personally completed 30 minutes of Motionbuilder previs & blockout  through to final camera polish.</li>
+                                    <li>Our team completed over 2 hours of highly polished narrative cinematic content.</li>
+                                    <li>Trusted with milestone scenes such as cinematic style-guide, quality bar benchmark, and vertical-slice.</li>
+                                    <li>Carried out animation polish notes, performance modification, and bug fixes.</li>
+                                    <li>Provided polish passes on additional layout content for impact, punch, or clarity.</li>
+                                    <li>Used Nuke to tackle additional post effects.</li>
+                                    <li>Pre-production work including storyboards, animatics and proof of concept videos.</li>
+                                </ul>
+                            </div>
+                            <div className="job-duties">
+                                <h5>Tech Art</h5>
+                                <ul className="duties-list">
+                                    <li>Developed the first Motionbuilder to Firebird Engine export pipeline tool including front-end UI and supplementary processing scripts.  </li>
+                                    <li>Developed and maintained many additional Motionbuilder tools to assist with rapid scene iteration.</li>
+                                    <li>Overhauled our offline rendering utility “Renderbot” to make it user friendly, faster, more versatile and reliable.</li>
+                                    <li>Developed and maintained an updated camera rig and camera toolset used team-wide.</li>
+                                    <li>Key point of contact and support with Autodesk in the development of a custom WB Motionbuilder build to better suit the team’s needs.</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="game-details">
+                            <h4 className="game-name">“Middle Earth: Shadow of War - The Blade of Galadriel" (DLC)</h4>
+                                <div className="job-duties">
+                                    <h5>Content</h5>
+                                    <ul className="duties-list">
+                                        <li>Motionbuilder previs & blockout through to final camera polish.</li>
+                                        <li>Additional body, creature, and prop animation in my layouts.</li>
+                                    </ul>
+                                </div>
+                        </div>
+                        <div className="game-details">
+                            <h4 className="game-name">“Middle Earth: Shadow of War - Desolation of Mordor" (DLC)</h4>
+                                <div className="job-duties">
+                                    <h5>Content</h5>
+                                    <ul className="duties-list">
+                                        <li>Motionbuilder previs & blockout through to final camera polish.</li>
+                                        <li>Completed all body and prop animation in my layouts.</li>
+                                        <li>Used Nuke and AfterEffects for a few post effects.</li>
+                                    </ul>
+                                </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="position-section">
                     <h3>Warner Bros / Monolith Productions</h3>
