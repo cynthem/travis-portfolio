4fa1e29c608933f51541cdf85bd7a7b31e9cfdfb
doozles411 <cindy.phelps01@gmail.com>
2024-01-31 17:01:49 -0800

    Add contact details

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -9,7 +9,36 @@ export default function Contact() {
                 <div className="transition-overlay"></div>
             </Parallax>
             <div className="contact-content">
-                content here
+                <div className="contact-row">
+                    <p className="contact-type">EMAIL</p>
+                    <a href="mailto:travis@travisphelps.com">travis@travisphelps.com</a>
+                </div>
+                <div className="contact-row">
+                    <p className="contact-type">PHONE</p>
+                    <p className="phone">(253) 693-8362</p>
+                </div>
+                <div className="contact-row">
+                    <p className="contact-type">LINKEDIN</p>
+                    <a 
+                        className="linkedin"
+                        href="https://www.linkedin.com/in/travisphelps"
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        LINKEDIN.COM/IN/TRAVISPHELPS
+                    </a>
+                </div>
+                <div className="contact-row">
+                    <p className="contact-type">INSTAGRAM</p>
+                    <a 
+                        className="instagram"
+                        href="https://www.instagram.com/tjpdigital"
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        TJPDIGITAL
+                    </a>
+                </div>
             </div>
         </div>
     );
