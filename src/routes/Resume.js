7291a8ce72101b7a049d16274da01d13af10dbe0
doozles411 <cindy.phelps01@gmail.com>
2024-01-31 16:20:59 -0800

    Add overlay styles

--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Parallax } from "react-parallax";
+import resumeBackground from "../assets/images/art/Resume_Background_1280.png";
 
 export default function Resume() {
     return (
         <div className="resume">
-            resume
+            <Parallax className="resume-image" bgImage={resumeBackground} strength={400}>
+                <div className="transition-overlay">test</div>
+            </Parallax>
+            <div className="resume-content">
+                testing 123
+            </div>
         </div>
     );
 }
