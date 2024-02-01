2e3c62826649bba726529ced558b5f5231a751f1
doozles411 <cindy.phelps01@gmail.com>
2024-01-31 16:33:25 -0800

    Add initial content

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Parallax } from "react-parallax";
+import contactBackground from "../assets/images/art/Contact_Background.png";
 
 export default function Contact() {
     return (
         <div className="contact">
-            contact
+            <Parallax className="contact-image" bgImage={contactBackground} strength={400}>
+                <div className="transition-overlay"></div>
+            </Parallax>
+            <div className="contact-content">
+                content here
+            </div>
         </div>
     );
 }
