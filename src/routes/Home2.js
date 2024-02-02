c3eb419cfc29bb6d0aea254aefe0c3e990076529
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 15:49:05 -0800

    Create and export project_2 list

--- src/routes/Home2.js
+++ src/routes/Home2.js
@@ -2,20 +2,6 @@ import React from "react";
 import uniqid from "uniqid";
 import ProjectContainer from "../components/ProjectContainer";
 import projectList from "../util/projectList";
-import Project1 from "../assets/images/Home_Background.png";
-import Project2 from "../assets/images/2018_Spring_1280.png";
-import project_1 from "../util/project_1";
-
-const parallaxImages = [
-    {
-        parallax: Project1,
-        projects: project_1
-    },
-    {
-        parallax: Project2,
-        projects:
-    }
-];
 
 export default function Home() {
     return (
