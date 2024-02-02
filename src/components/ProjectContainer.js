4bf4a4ae3b5ad73fdef8fe55e506635465019432
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 14:12:24 -0800

    Update ProjectContainer import

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Parallax } from "react-parallax";
+import uniqid from "uniqid";
 import Projects from "./Projects";
 
 export default function ProjectContainer({ project }) {
