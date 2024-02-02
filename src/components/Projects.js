3e77af4cae723239f94fb3bbd9f10d98173f48bc
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 14:30:35 -0800

    Render project previews

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,52 +1,32 @@
 import React from "react";
 
-export default function Projects() {
+export default function Projects({ preview }) {
     const overlayOpacity = {
-        backgroundColor: project.opacity1 || 'rgba(0, 0, 0, 0.3)'
+        backgroundColor: preview.project.opacity || 'rgba(0, 0, 0, 0.3)'
     }
 
     return (
-        <div className="projects"></div>
+        <div className="project">
+            <a
+                className="description-link"
+                href={preview.project.link}
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                <img 
+                    className="link-image"
+                    src={preview.project.image} 
+                    alt="Project image" 
+                />
+            </a>
+            <div className="image-overlay" style={overlayOpacity}></div>
+            <a className="description-text"
+                href={preview.project.link}
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                {preview.project.name}
+            </a>
+        </div>
     );
 }
-
-<div className="project-description">
-                <div className="description-details">
-                    <a 
-                        className="description-link"
-                        href={project.link1}
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        <img 
-                            className="link-image"
-                            src={project.image1} 
-                            alt="Project image" 
-                        />
-                    </a>
-                    <div className="image-overlay" style={overlayOpacity1}></div>
-                    <a className="description-text"
-                        href={project.link1}
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        {project.name1}
-                    </a>
-                </div>
-                <div className="description-details">
-                    <a
-                        className="description-link"
-                        href={project.link2}
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        <img 
-                            className="link-image" 
-                            src={project.image2} 
-                            alt="Project image" 
-                        />
-                    </a>
-                    <div className="image-overlay" style={overlayOpacity2}></div>
-                    <p className="description-text">{project.name2}</p>
-                </div>
-            </div>
