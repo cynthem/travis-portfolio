442209dae05b255fa49a97cba5585a4e44a8bb33
doozles411 <cindy.phelps01@gmail.com>
2024-02-03 12:29:04 -0800

    Add IconContext

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { IconContext } from "react-icons";
 
@@ -10,46 +10,48 @@ export default function NavBar() {
     const closeMobileMenu = () => setClick(false);
 
     return (
-        <div className="header-navigation">
-            <div className="menu-icon" onClick={handleClick}>
-                {click ? <RxCross1 /> : <RxHamburgerMenu />}
-            </div>
+        <IconContext.Provider value={{ color: "#d3d3d3" }}>
+            <div className="header-navigation">
+                <div className="menu-icon" onClick={handleClick}>
+                    {click ? <RxCross1 /> : <RxHamburgerMenu />}
+                </div>
 
-            <ul className={click ? "nav-menu active" : "nav-menu"}> 
-                <li className="nav-item">
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            "nav-links" + (isActive ? " activated" : "")
-                        }
-                        onClick={closeMobileMenu}
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink
-                        to="/resume"
-                        className={({ isActive }) =>
-                            "nav-links" + (isActive ? " activated" : "")
-                        }
-                        onClick={closeMobileMenu}
-                    >
-                        Resume
-                    </NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink
-                        to="/contact"
-                        className={({ isActive }) =>
-                            "nav-links" + (isActive ? " activated" : "")
-                        }
-                        onClick={closeMobileMenu}
-                    >
-                        Contact
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}> 
+                    <li className="nav-item">
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/resume"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
+                            Resume
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            to="/contact"
+                            className={({ isActive }) =>
+                                "nav-links" + (isActive ? " activated" : "")
+                            }
+                            onClick={closeMobileMenu}
+                        >
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </IconContext.Provider>
     );
 }
