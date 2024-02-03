95b0bf22611a483f45b8f5998658274b1e8c3803
doozles411 <cindy.phelps01@gmail.com>
2024-02-03 12:22:57 -0800

    Add nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,52 @@ import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
 import { IconContext } from "react-icons";
 
 export default function NavBar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <div className="header-navigation">
-            
+            <div className="menu-icon" onClick={handleClick}>
+                {click ? <RxCross1 /> : <RxHamburgerMenu />}
+            </div>
+
+            <ul className={click ? "nav-menu active" : "nav-menu"}> 
+                <li className="nav-item">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            "nav-links" + (isActive ? " activated" : "")
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink
+                        to="/resume"
+                        className={({ isActive }) =>
+                            "nav-links" + (isActive ? " activated" : "")
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Resume
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink
+                        to="/contact"
+                        className={({ isActive }) =>
+                            "nav-links" + (isActive ? " activated" : "")
+                        }
+                        onClick={closeMobileMenu}
+                    >
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
         </div>
     );
 }
