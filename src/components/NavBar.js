f0eaeb472ed8073b609455ef9901fa1f9024d836
doozles411 <cindy.phelps01@gmail.com>
2024-02-03 12:13:37 -0800

    Add outer div

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Link } from "react-router-dom";
 import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { IconContext } from "react-icons";
+
+export default function NavBar() {
+    return (
+        <div className="header-navigation">
+            
+        </div>
+    );
+}
