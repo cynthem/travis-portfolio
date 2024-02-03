a27b77a0a2d8062054219219e4bf03f42d8e8bb9
doozles411 <cindy.phelps01@gmail.com>
2024-02-03 13:11:36 -0800

    Move App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./routes/App";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import ErrorPage from "./routes/ErrorPage";
 import Home from "./routes/Home";
 import Resume from "./routes/Resume";
 import Contact from "./routes/Contact";
 import "./sass/main.scss";
+
+const App = () => {
+    return (
+        <div className="App">
+
+            <div className="header">
+                <div className="header-title">
+                    <h1>TRAVIS PHELPS</h1>
+                    <h2>CINEMATIC ARTIST</h2>
+                </div>
+                <NavBar />
+            </div>
+
+            <div className="main-content"><Outlet /></div>
+
+            <div className="footer">
+                <p>© 2024 TRAVIS PHELPS</p>
+                <p>Made by 
+                    <a 
+                        className="footer-link"
+                        href='https://github.com/cynthem' 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        CMP
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+}
 
 const router = createBrowserRouter([
     {
---
+++ src/tempindex.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./routes/App";
+import ErrorPage from "./routes/ErrorPage";
+import Home from "./routes/Home";
+import Resume from "./routes/Resume";
+import Contact from "./routes/Contact";
+import "./sass/main.scss";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: "home",
+                element: <Home />
+            },
+            {
+                path: "resume",
+                element: <Resume />
+            },
+            {
+                path: "contact",
+                element: <Contact />
+            }
+        ]
+    }
+]);
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+root.render(<RouterProvider router={router} />);
