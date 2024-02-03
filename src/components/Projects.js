e04846d6e6411a07e08adc7073439268eadc662d
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 16:05:46 -0800

    Change prop name

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,31 +1,31 @@
 import React from "react";
 
-export default function Projects({ parts }) {
+export default function Projects({ preview }) {
     const overlayOpacity = {
-        backgroundColor: parts.opacity || 'rgba(0, 0, 0, 0.3)'
+        backgroundColor: preview.opacity || 'rgba(0, 0, 0, 0.3)'
     }
 
     return (
         <div className="project">
             <a
                 className="description-link"
-                href={parts.link}
+                href={preview.link}
                 target="_blank" 
                 rel="noopener noreferrer"
             >
                 <img 
                     className="link-image"
-                    src={parts.image} 
+                    src={preview.image} 
                     alt="Project image" 
                 />
             </a>
             <div className="image-overlay" style={overlayOpacity}></div>
             <a className="description-text"
-                href={parts.link}
+                href={preview.link}
                 target="_blank" 
                 rel="noopener noreferrer"
             >
-                {parts.name}
+                {preview.name}
             </a>
         </div>
     );
