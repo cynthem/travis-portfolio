91c4b4ff1db0d1c8c61b138c696af2d5c77ed2bb
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 16:00:07 -0800

    Refactor code

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,36 +1,17 @@
 import React from "react";
-import { Parallax } from "react-parallax";
 import uniqid from "uniqid";
 import ProjectContainer from "../components/ProjectContainer";
-import Project1 from "../assets/images/Home_Background.png";
-import Project2 from "../assets/images/2018_Spring_1280.png";
-import project_1 from "../util/project_1";
-import project_2 from "../util/project_2";
-
-const parallaxImages = [
-    {
-        parallax: Project1,
-        projects: project_1
-    },
-    {
-        parallax: Project2,
-        projects: project_2
-    }
-];
+import projectList from "../util/projectList";
 
 export default function Home() {
     return (
         <div className="home">
-            {parallaxImages.map((project) => {
+            {projectList.map((projects) => {
                 return (
-                    <div className="project-container">
-                        <Parallax className="project-image" bgImage={project.parallax} strength={400}></Parallax>
-
-                        <ProjectContainer
-                            key={uniqid()} 
-                            projects={project.projects}
-                        />
-                    </div>
+                    <ProjectContainer
+                        key={uniqid()} 
+                        projects={projects}
+                    />
                 );
             })}
         </div>
