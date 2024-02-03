7d6de99ac66640e70d3071a7d3e47b0fa9f305ee
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 16:03:02 -0800

    Revert prop name'

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -3,7 +3,7 @@ import { Parallax } from "react-parallax";
 import uniqid from "uniqid";
 import Projects from "./Projects";
 
-export default function ProjectContainer({ projects }) {
+export default function ProjectContainer({ previews }) {
     return (
         <div className="project-container">
             <Parallax className="project-image" bgImage={projects.bkgrd} strength={400}></Parallax>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
                 return (
                     <ProjectContainer
                         key={uniqid()} 
-                        previews={projects.previews}
+                        projects={projects}
                     />
                 );
             })}
