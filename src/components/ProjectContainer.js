4ff1007e2f9b2ef3815f5656ef448c0fb06d731c
doozles411 <cindy.phelps01@gmail.com>
2024-02-02 16:05:14 -0800

    Pass preview prop

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -3,13 +3,20 @@ import { Parallax } from "react-parallax";
 import uniqid from "uniqid";
 import Projects from "./Projects";
 
-export default function ProjectContainer({ previews }) {
+export default function ProjectContainer({ projects }) {
     return (
         <div className="project-container">
             <Parallax className="project-image" bgImage={projects.bkgrd} strength={400}></Parallax>
 
             <div className="projects-display">
-                {}
+                {projects.previews.map((preview) => {
+                    return (
+                        <Projects 
+                            key={uniqid()}
+                            preview={preview}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
