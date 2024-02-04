b55a417ce4b26861be6cfde5155282fa6e9453ac
doozles411 <cindy.phelps01@gmail.com>
2024-02-04 14:35:09 -0800

    Add small image sizes

--- src/util/projectList.js
+++ src/util/projectList.js
@@ -1,17 +1,21 @@
-import homeMain from "../assets/images/Home_Background.png";
-import homeMainMed from "../assets/images/Home_Background_1020.png";
-import homeMainSm from "../assets/images/Home_Background_900.png";
-import Spring2018 from "../assets/images/2018_Spring_1280.png";
-import Spring2018Med from "../assets/images/2018_Spring_1020.png";
-import Spring2018Sm from "../assets/images/2018_Spring_900.png";
+import top1280 from "../assets/images/Home_Background.png";
+import top1020 from "../assets/images/Home_Background_1020.png";
+import top900 from "../assets/images/Home_Background_900.png";
+import top750 from "../assets/images/Home_Background_750.png";
+import Spring2018_1280 from "../assets/images/2018_Spring_1280.png";
+import Spring2018_1020 from "../assets/images/2018_Spring_1020.png";
+import Spring2018_900 from "../assets/images/2018_Spring_900.png";
+import Spring2018_750 from "../assets/images/2018_Spring_750.png";
 import Spring2018Ext from "../assets/images/2018_Ext_Spring_1280.png";
-import Summer2017 from "../assets/images/2017_Summer_1280.png";
-import Summer2017Med from "../assets/images/2017_Summer_1020.png";
-import Summer2017Sm from "../assets/images/2017_Summer_900.png";
+import Summer2017_1280 from "../assets/images/2017_Summer_1280.png";
+import Summer2017_1020 from "../assets/images/2017_Summer_1020.png";
+import Summer2017_900 from "../assets/images/2017_Summer_900.png";
+import Summer2017_750 from "../assets/images/2017_Summer_900.png";
 import Summer2017Ext from "../assets/images/2017_Ext_Summer_1280.png";
-import Winter2015 from "../assets/images/2015_Winter_1280.png";
-import Winter2015Med from "../assets/images/2015_Winter_1020.png";
-import Winter2015Sm from "../assets/images/2015_Winter_900.png";
+import Winter2015_1280 from "../assets/images/2015_Winter_1280.png";
+import Winter2015_1020 from "../assets/images/2015_Winter_1020.png";
+import Winter2015_900 from "../assets/images/2015_Winter_900.png";
+import Winter2015_750 from "../assets/images/2015_Winter_750.png";
 import Winter2015Ext from "../assets/images/2015_Ext_Winter_1280.png";
 import Summer2012 from "../assets/images/2012_Summer_1280.png";
 import Summer2012Anim from "../assets/images/2012_Anim_Summer_1280.png";
@@ -19,9 +23,10 @@ import Summer2012Anim from "../assets/images/2012_Anim_Summer_1280.png";
 
 const projectList = [
     {
-        bkgrdLg: homeMain,
-        bkgrdMed: homeMainMed,
-        bkgrdSm: homeMainSm,
+        bkgrd1280: top1280,
+        bkgrd1020: top1020,
+        bkgrd900: top900,
+        bkgrd750: top750,
         previews: [
             {
                 name: "CURRENT", 
@@ -32,9 +37,10 @@ const projectList = [
         ]
     },
     {
-        bkgrdLg: Spring2018,
-        bkgrdMed: Spring2018Med,
-        bkgrdSm: Spring2018Sm,
+        bkgrd1280: Spring2018_1280,
+        bkgrd1020: Spring2018_1020,
+        bkgrd900: Spring2018_900,
+        bkgrd750: Spring2018_750,
         previews: [
             {
                 name: "SPRING 2018", 
@@ -51,9 +57,10 @@ const projectList = [
         ] 
     },
     {
-        bkgrdLg: Summer2017,
-        bkgrdMed: Summer2017Med,
-        bkgrdSm: Summer2017Sm,
+        bkgrd1280: Summer2017_1280,
+        bkgrd1020: Summer2017_1020,
+        bkgrd900: Summer2017_900,
+        bkgrd750: Summer2017_750,
         previews: [
             {
                 name: "SUMMER 2017", 
@@ -70,9 +77,10 @@ const projectList = [
         ] 
     },
     {
-        bkgrdLg: Winter2015,
-        bkgrdMed: Winter2015Med,
-        bkgrdSm: Winter2015Sm,
+        bkgrd1280: Winter2015_1280,
+        bkgrd1020: Winter2015_1020,
+        bkgrd900: Winter2015_900,
+        bkgrd750: Winter2015_750,
         previews: [
             {
                 name: "WINTER 2015", 
@@ -89,9 +97,10 @@ const projectList = [
         ]
     },
     /*{
-        bkgrdLg: Summer2012,
-        bkgrdMed: ,
-        bkgrdSm: ,
+        bkgrd1280: Summer2012,
+        bkgrd1020: ,
+        bkgrd900: ,
+        bkgrd750: ,
         previews: [
             {
                 name: "SUMMER 2012", //might need text: "password protected"
