2a1e4a66945dff4ff91a05fff95fed5c23778595
doozles411 <cindy.phelps01@gmail.com>
2024-02-04 12:21:25 -0800

    Create hook for responsive images

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Parallax } from "react-parallax";
 import uniqid from "uniqid";
 import Projects from "./Projects";
 
 export default function ProjectContainer({ projects }) {
+    const [screenSize, setScreenSize] = useState(projects.bkgrdLg);
 
-    
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth >= 1250) {
+                setScreenSize(projects.bkgrdLg);
+            } else if (window.innerWidth < 1250) {
+                setScreenSize(projects.bkgrdMed);
+            }
+        }
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
     return (
         <div className="project-container">
             <Parallax 
                 className="project-image" 
-                bgImage={projects.bkgrd}
+                bgImage={screenSize}
                 strength={400}
             >
             </Parallax>
