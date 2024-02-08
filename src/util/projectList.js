91947cf1015733a4dec2e530ef291393869d1b19
doozles411 <cindy.phelps01@gmail.com>
2024-02-08 15:39:12 -0800

    Add mobile images

--- src/util/projectList.js
+++ src/util/projectList.js
@@ -3,23 +3,27 @@ import top1020 from "../assets/images/Home_Background_1020.png";
 import top900 from "../assets/images/Home_Background_900.png";
 import top750 from "../assets/images/Home_Background_750.png";
 import top600 from "../assets/images/Home_Background_600.png";
+import top450 from "../assets/images/Home_Background_450.png";
 import Spring2018_1280 from "../assets/images/2018_Spring_1280.png";
 import Spring2018_1020 from "../assets/images/2018_Spring_1020.png";
 import Spring2018_900 from "../assets/images/2018_Spring_900.png";
 import Spring2018_750 from "../assets/images/2018_Spring_750.png";
 import Spring2018_600 from "../assets/images/2018_Spring_600.png";
+import Spring2018_450 from "../assets/images/2018_Spring_450.png";
 import Spring2018Ext from "../assets/images/2018_Ext_Spring_1280.png";
 import Summer2017_1280 from "../assets/images/2017_Summer_1280.png";
 import Summer2017_1020 from "../assets/images/2017_Summer_1020.png";
 import Summer2017_900 from "../assets/images/2017_Summer_900.png";
 import Summer2017_750 from "../assets/images/2017_Summer_750.png";
 import Summer2017_600 from "../assets/images/2017_Summer_600.png";
+import Summer2017_450 from "../assets/images/2017_Summer_450.png";
 import Summer2017Ext from "../assets/images/2017_Ext_Summer_1280.png";
 import Winter2015_1280 from "../assets/images/2015_Winter_1280.png";
 import Winter2015_1020 from "../assets/images/2015_Winter_1020.png";
 import Winter2015_900 from "../assets/images/2015_Winter_900.png";
 import Winter2015_750 from "../assets/images/2015_Winter_750.png";
 import Winter2015_600 from "../assets/images/2015_Winter_600.png";
+import Winter2015_450 from "../assets/images/2015_Winter_450.png";
 import Winter2015Ext from "../assets/images/2015_Ext_Winter_1280.png";
 import Summer2012 from "../assets/images/2012_Summer_1280.png";
 import Summer2012Anim from "../assets/images/2012_Anim_Summer_1280.png";
@@ -32,6 +36,7 @@ const projectList = [
         bkgrd900: top900,
         bkgrd750: top750,
         bkgrd600: top600,
+        bkgrd450: top450,
         previews: [
             {
                 name: "CURRENT", 
@@ -47,6 +52,7 @@ const projectList = [
         bkgrd900: Spring2018_900,
         bkgrd750: Spring2018_750,
         bkgrd600: Spring2018_600,
+        bkgrd450: Spring2018_450,
         previews: [
             {
                 name: "SPRING 2018", 
@@ -68,6 +74,7 @@ const projectList = [
         bkgrd900: Summer2017_900,
         bkgrd750: Summer2017_750,
         bkgrd600: Summer2017_600,
+        bkgrd450: Summer2017_450,
         previews: [
             {
                 name: "SUMMER 2017", 
@@ -89,6 +96,7 @@ const projectList = [
         bkgrd900: Winter2015_900,
         bkgrd750: Winter2015_750,
         bkgrd600: Winter2015_600,
+        bkgrd450: Winter2015_450,
         previews: [
             {
                 name: "WINTER 2015", 
@@ -110,6 +118,7 @@ const projectList = [
         bkgrd900: ,
         bkgrd750: ,
         bkgrd600: ,
+        bkgrd450: ,
         previews: [
             {
                 name: "SUMMER 2012", //might need text: "password protected"
