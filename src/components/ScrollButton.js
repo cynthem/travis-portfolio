68ddde82f2614b38f469669a2d049a712a5a483f
doozles411 <cindy.phelps01@gmail.com>
2024-02-09 14:55:14 -0800

    Create button component

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function ScrollButton() {
-    return (
-        <div className="scroll-button">
+    const scrollToTop = () => {
+        window.scrollTo({ 
+            top: 0,  
+            behavior: 'smooth'
+        });
+    }
 
+    return (
+        <div className="bottom-scroll"
+            onClick={scrollToTop}
+        >
+            <button className="scroll-button">
+                Scroll to Top
+            </button>
         </div>
     );
 }
