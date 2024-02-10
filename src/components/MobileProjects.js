2b168b0f6358b5d8442b74192c38b7740e9a12f5
doozles411 <cindy.phelps01@gmail.com>
2024-02-10 15:17:51 -0800

    Test mobile scroll effect

--- src/components/MobileProjects.js
+++ src/components/MobileProjects.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Parallax } from "react-parallax";
 import uniqid from "uniqid";
 import Projects from "./Projects";
 
 export default function MobileProjects({ projects }) {
     return (
         <div className="project-container">
-            <div 
+            
+            <Parallax 
                 className="mobile-background" 
-                style={{ backgroundImage: `url("${projects.bkgrd450}")` }}
+                bgImage={projects.bkgrd450}
+                //strength={0}
             >
-            </div>
+            </Parallax>
 
             <div className="projects-display">
                 {projects.previews.map((preview) => {
