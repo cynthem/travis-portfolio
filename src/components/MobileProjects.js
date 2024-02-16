3c141443a81cb35a8f54d61a828bc1d4cae23798
doozles411 <cindy.phelps01@gmail.com>
2024-02-15 17:31:04 -0800

    Adjust mobile layout

--- src/components/MobileProjects.js
+++ src/components/MobileProjects.js
@@ -4,14 +4,8 @@ import Projects from "../components/Projects";
 
 export default function MobileProjects({ projects }) {
     return (
-        <div className="project-container">
-            <div 
-                className="mobile-background" 
-                style={{ backgroundImage: `url("${projects.bkgrd450}")` }}
-            >
-            </div>
-
-            <div className="projects-display">
+        <div className="mobile-project-container">
+            <div className="mobile-projects-display">
                 {projects.previews.map((preview) => {
                     return (
                         <Projects 
