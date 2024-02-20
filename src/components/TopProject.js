3d1e9b37c0a2d1d5639f9182e81f98c5de2cd822
doozles411 <cindy.phelps01@gmail.com>
2024-02-19 16:51:09 -0800

    Export TopProject

---
+++ src/components/TopProject.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function TopProject({ preview }) {
+    const overlayOpacity = {
+        backgroundColor: preview.opacity || 'rgba(0, 0, 0, 0.3)'
+    }
+
+    return (
+        <div className="project">
+            <a
+                className="description-link"
+                href={preview.link}
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                <img 
+                    className="link-image"
+                    src={preview.image} 
+                    alt="Cinematic scene" 
+                />
+            </a>
+            <div className="image-overlay" style={overlayOpacity}></div>
+            <a className="description-text"
+                href={preview.link}
+                target="_blank" 
+                rel="noopener noreferrer"
+            >
+                {preview.name}
+            </a>
+        </div>
+    );
+}
